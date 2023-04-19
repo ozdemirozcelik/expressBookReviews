4b591f8008302dde3671ee8f2bb008e7463056c0
ozdemirozcelik <ozdemirozcelik@gmail.com>
2023-04-19 10:54:51 -0700

    final

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-const axios = require('axios');
+const axios = require('axios').default;
 
 
 public_users.post("/register", (req,res) => {
@@ -25,16 +25,14 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
   //Write your code here
-  
-  res.send(books);// returns proper JSON
+  res.status(200).json(books);
 
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
-  res.send(books[isbn]);// returns proper JSON
-  // res.send(JSON.stringify(books[isbn],null,4));
+  res.status(200).json(books[isbn]);
  });
   
 
@@ -50,9 +48,7 @@ public_users.get('/author/:author',function (req, res) {
       returnbooks[id] = books[id]
     }
   }
-
-  res.send(returnbooks);// returns proper JSON
-  // res.send(JSON.stringify(returnbooks,null,4));
+  res.status(200).json(returnbooks);
 });
 
 // Get all books based on title
@@ -68,16 +64,62 @@ public_users.get('/title/:title',function (req, res) {
     }
   }
 
-  res.send(returnbooks); // returns proper JSON
-  // res.send(JSON.stringify(returnbooks,null,4));
+  res.status(200).json(returnbooks);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  res.send(books[isbn]['reviews']); // returns proper JSON
-  // res.send(JSON.stringify(books[isbn]['reviews'],null,4));
+  res.status(200).json(books[isbn]['reviews']);
 });
 
 module.exports.general = public_users;
+
+
+//Task 10-11-12-13 AXIOS
+
+// TASK 10 Get the book list available in the shop
+public_users.get("/async", async (req, res) => {
+	try {
+
+    const books = await axios.get("http://localhost:5000")
+		res.status(200).json(books.data);
+    // res.send(books);
+	} catch (err) {
+		res.status(500).json({ message: err });
+	}
+});
+
+// TASK 11 Get book details based on ISBN
+public_users.get("/async/isbn/:isbn", async (req, res) => {
+	try {
+    const isbn = req.params.isbn;
+    const book = await axios.get(`http://localhost:5000/isbn/${isbn}`)
+		res.status(200).json(book.data);
+	} catch (err) {
+		res.status(500).json({ message: err });
+	}
+});
+
+// TASK 12  // Get book details based on author
+public_users.get("/async/author/:author", async (req, res) => {
+	try {
+    const author = req.params.author;
+    const books = await axios.get(`http://localhost:5000/author/${author}`)
+		res.status(200).json(books.data);
+	} catch (err) {
+		res.status(500).json({ message: err });
+	}
+});
+
+// TASK 13  // Get book details based on title
+public_users.get("/async/title/:title", async (req, res) => {
+	try {
+    const title = req.params.title;
+    const books = await axios.get(`http://localhost:5000/title/${title}`)
+		res.status(200).json(books.data);
+	} catch (err) {
+		res.status(500).json({ message: err });
+	}
+});
